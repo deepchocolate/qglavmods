44f39acadb41b24e579548f81bfb42e976fbdc84
deepchocholate <andreas@systerdyster.se>
2021-02-26 16:38:52 +0100

    Initial commit

---
+++ R/bivtwinmod.R
@@ -0,0 +1,99 @@
+#' Generate syntax for a bivariate twin model
+#'
+#' @param measT1 A list of variable name and a designated label eg label(varName1="label1") for measure 1.
+#' @param measT2 A list of variable name and a designated label eg label(varName1="label2") for measure 2.
+#' @param regressions A list of lists for the regressions for measT1 and measT1.
+#' @param model A list of variance components (A,C,D,E) and labels, eg list(A='a',C='c',E='e').
+bivTwinMod <- function (measT1, measT2,regressions=list(list(int=1,fem='female'),list(int=1,fem='female')), model=list(A='a',C='c',E='e')) {
+  corrs <- list(A=c(0.5,1),C=c(1,1),D=c(0.25,1))
+  defs <- c()
+  defsVar <- list()
+  defsCorr <- list()
+  m <- '# Traits in twins are assumed to be uncorrelated\n'
+  for (i in 1:2) m <- paste0(m,names(measT1[i]),' ~~ 0*',names(measT2[i]),'\n');
+  m <- paste0(m,'# Regressions\n')
+  for (i in 1:2) {
+    reg <- c()
+    for (x in names(regressions[[i]])) {
+      reg <- c(reg,paste0('c(',x,',',x,')*',regressions[[i]][[x]],ifelse(!is.numeric(regressions[[i]][[x]]),'_1','')))
+    }
+    reg <- paste(reg,collapse='+')
+    m <- paste0(m,names(measT1[i]),' ~ ',paste(reg,collapse=' + '),'\n')
+    reg <- c()
+    for (x in names(regressions[[i]])) reg <- c(reg,paste0('c(',x,',',x,')*',regressions[[i]][[x]],ifelse(!is.numeric(regressions[[i]][[x]]),'_2','')))
+    reg <- paste(reg,collapse='+')
+    m <- paste0(m,names(measT2[i]),' ~ ',paste(reg,collapse=' + '),'\n')
+  }
+  for (fac in names(model)) {
+    defsCorr[[fac]] <- c()
+    if (fac == 'A') {m <- paste0(m,'# Genetics\n');}
+    else if (fac== 'C') {m <- paste0(m,'# Shared environment\n');}
+    else if (fac == 'E') {m <- paste0(m,'# Unique environment\n');}
+    m <- paste0(m,'# Twin 1 measures\n')
+    for (out in names(measT1)) {
+      paraLab <- paste0(model[fac],measT1[out])
+      m <- paste0(m,fac,out,' =~ c(',paraLab,',',paraLab,')*',out,'\n')
+    }
+    corr <- paste(corrs[fac],collapse=',')
+    if (fac == 'E') corr <- 0;
+    m <- paste0(m, fac,names(measT1[1]),' ~~ ',corr,'*',fac,names(measT2[1]),'\n')
+    m <- paste0(m, '# Twin 2 measures\n')
+    for (out in names(measT2)) {
+      paraLab <- paste0(model[fac],measT2[out])
+      m <- paste0(m,fac,out,' =~ c(',paraLab,',',paraLab,')*',out,'\n')
+    }
+    corr <- paste(corrs[fac],collapse=',')
+    if (fac == 'E') corr <- 0;
+    m <- paste0(m, fac,names(measT1[2]),' ~~ ',corr,'*',fac,names(measT2[2]),'\n');
+    m <- paste0(m,'# Cross-twin cross-trait correlations\n')
+    corrParam <- paste0('*r',fac)
+    corrParam <- paste0('c(',paste0(corrs[[fac]],c(corrParam,corrParam),collapse=','),')')
+    if (fac == 'E') corrParam <- 0;
+    if (fac == 'C') corrParam <- paste0('c(r',fac,',r',fac,')');
+    #for (i in 1:2) {
+    m <- paste0(m,fac,names(measT1[1]),' ~~ ',corrParam,'*',fac,names(measT2[2]),'\n')
+    m <- paste0(m,fac,names(measT1[2]),' ~~ ',corrParam,'*',fac,names(measT2[1]),'\n')
+    #}
+    m <- paste0(m,'# Cross-trait correlations\n')
+    corrParam <- paste0('c(','r',fac,',r',fac,')')
+    #if (fac == 'E') corrParam <- 0;
+    m <- paste0(m,fac,names(measT1[1]),' ~~ ',corrParam,'*',fac,names(measT1[2]),'\n')
+    m <- paste0(m,fac,names(measT2[1]),' ~~ ',corrParam,'*',fac,names(measT2[2]),'\n')
+    for (i in 1:2) {
+      if (is.null(defsVar[[measT1[[i]]]])) defsVar[measT1[[i]]] <- c()
+      corrParam <- paste0(model[fac],measT1[i])
+      sqParam <- paste0(corrParam,'SQ')
+      defs <- c(defs, paste0(corrParam,'SQ := ',corrParam,'*',corrParam))
+      defsVar[[measT1[[i]]]] <- c(defsVar[[measT1[[i]]]],sqParam)
+      defsCorr[[fac]] <- c(defsCorr[[fac]],corrParam)
+    }
+  }
+  m <- paste0(m,'# Defined parameters\n# Squared correlations\n')
+  m <- paste0(m,paste0(defs,collapse='\n'),'\n')
+  m <- paste0(m,'# Variance explained in each trait\n')
+  #print(defsVar)
+  for (x in names(defsVar)) {
+    tot <- paste0(defsVar[[x]],collapse='+')
+    for (y in defsVar[[x]]) {
+      sh <- paste0(y,'_share := ')
+      m <- paste0(m,sh,y,'/(', tot,')\n')
+    }
+    #m <- paste0(m,sh,'\n')
+  }
+  m <- paste0(m, '# Genetic and environmental correlations between traits\n')
+  # Add these definitions so that all parameters are easily accessible through the lhs column in results
+  m <- paste0(m,'corr_A := rA\ncorr_C := rC\ncorr_E := rE\n')
+  m <- paste0(m,'# Genetic and environmental contributions to phenotypic correlations\n')
+  corrLabs <- c()
+  for (x in names(defsCorr)) {
+    corrLab <- paste0('contr_',x)
+    corrLabs <- c(corrLabs,corrLab)
+    m <- paste0(m,corrLab,' := r',x,'*',paste0(defsCorr[[x]],collapse='*'),'\n')
+  }
+  tot <- paste0(corrLabs,collapse='+')
+  for (x in corrLabs) {
+    m <- paste0(m,x,'_share := ',x,'/(',tot,')','\n')
+  }
+  return(m)
+}
+
---
+++ R/univtwinmod.R
@@ -0,0 +1,58 @@
+#' Generate syntax for a univariate twin model
+#'
+#' @param measT1 Variable name of measurement in twin one.
+#' @param measT2 Variable name of measurement in twin two.
+#' @param model A list of variance components (A,C,D,E) and labels, eg list(A='a',C='c',E='e').
+#' @param append Any additional syntax.
+#' @param varLabels Currently not used.
+univTwinMod <- function (measT1, measT2, model=list(A='a',C='c',E='e'), append='',varLabels=list(A='add',C='com',D='dom',E='uni'),
+                            independentT1=list(int='1'),independentT2=list(int='1')) {
+  corrs <- list(A=c(0.5,1),C=c(1,1),D=c(0.25,1))
+  o <- '# Measurments are uncorrelated\n'
+  o <- paste0(measT1,'~~0*',measT2,'\n')
+  if (length(independentT1) > 0 | length(independentT2) > 0) {
+    o <- paste0(o,'# Regressions\n')
+    r <- c()
+    for (lab in names(independentT1)) {
+      r <- c(r,paste0('c(',lab,',',lab,')*',independentT1[lab]))
+    }
+    o <- paste0(o,measT1,' ~ ',paste(r,collapse=' + '),'\n')
+    r <- c()
+    for (lab in names(independentT2)) {
+      r <- c(r,paste0('c(',lab,',',lab,')*',independentT2[lab]))
+    }
+    o <- paste0(o,measT2,' ~ ',paste(r,collapse=' + '),'\n')
+  }
+  totalVars <- c()
+  for (fac in names(model)) {
+    fac1 <- paste0(fac,'1')
+    fac2 <- paste0(fac,'2')
+    facName <- model[fac]
+    totalVars <- c(totalVars,paste0(facName,'2'))
+    o <- paste0(o,fac1,' =~ c(',facName,',',facName,')*',measT1,'\n')
+    o <- paste0(o,fac2,' =~ c(',facName,',',facName,')*',measT2,'\n')
+    if (fac=='A') {
+      o <- paste0(o,fac1,' ~~ c(0.5,1)*',fac2,'\n')
+    }
+    if (fac=='C') {
+      o <- paste0(o,fac1,'~~1*',fac2,'\n')
+    }
+    if (fac == 'D') {
+      o <- paste0(o,fac1,' ~~ c(0.25,1)*',fac2,'\n')
+    }
+    if (fac=='E') {
+      # Residual variation is uncorrelated
+      o <- paste0(o,fac1,'~~0*',fac2,'\n')
+    }
+    o <- paste0(o,facName,'2 := ',facName,'*',facName,'\n')
+  }
+  totVar <- paste0('(',paste0(totalVars,collapse='+'),')')
+  geneFacs <- c()
+  for (fac in names(model)) {
+    if (fac %in% c('A','D')) geneFacs <- c(geneFacs,paste0(model[fac],'2'))
+    o <- paste0(o,varLabels[fac],' := ',model[fac],'2/',totVar,'\n')
+  }
+  o <- paste0(o,'h2 := (',paste0(geneFacs,collapse='+'),')/',totVar,'\n')
+  return(paste0(o,append))
+}
+
